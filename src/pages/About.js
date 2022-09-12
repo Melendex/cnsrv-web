967c14631600bb94b9e3660f352e2c39332ec48a
SolorzanoLuis <wolfsolorzano2000@gmail.com>
2022-09-12 14:44:12 -0500

    Adición de los estilos del home y archivos por rutas

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from '../components//Header';
+import History from '../components//History';
+import Target from '../components//Target';
+import Target2 from '../components//Target2';
+import Footer from '../components//Footer';
+
+
+const About = () => {
+  return (
+    <>
+      <Header Title='Home' />
+        <Target />
+        <Target2 />
+        <History />
+      <Footer />
+    </>
+  )
+}
+
+export default About

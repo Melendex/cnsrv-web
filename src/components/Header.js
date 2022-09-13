a60b967a766b3869537728791de96c44c745db05
Melendex <omarmelendez638@gmail.com>
2022-09-12 23:27:34 -0500

    fix error navbar for deploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,44 +4,46 @@ import 'bootstrap/dist/css/bootstrap.css';
 import logo_cnsrv from './../img/Logos/logo_cnsrv_white.svg'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Link } from "react-router-dom";
+
 function Header({ Title }) {
     return (
 
         <header className="navbar-blue">
             <nav className="navbar navbar-light navbar-expand-md py-0">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="##">
+                    <Link className="navbar-brand" to="##">
                         <img src={logo_cnsrv} alt="" width="140" height="60" className="d-inline-block align-text-top" />
-                    </a>
+                    </Link>
                     <button className="navbar-toggler navbar-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse text-dark px-2" id="navbarText">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link" aria-current="page" href="/">Inicio</a>
+                                <Link className="nav-link" aria-current="page" to="/">Inicio</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/about">Nosotros</a>
+                                <Link className="nav-link" to="/about">Nosotros</Link>
                             </li>
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="##" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <Link className="nav-link dropdown-toggle" to="##" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Productos
-                                </a>
+                                </Link>
                                 <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item mdp" href="##">Mujeres de Palabra</a></li>
-                                    <li><a className="dropdown-item cta" href="##">Conserva T-activa</a></li>
-                                    <li><a className="dropdown-item tcc" href="##">Tu negocio con conserva</a></li>
+                                    <li><Link className="dropdown-item mdp" to="##">Mujeres de Palabra</Link></li>
+                                    <li><Link className="dropdown-item cta" to="##">Conserva T-activa</Link></li>
+                                    <li><Link className="dropdown-item tcc" to="##">Tu negocio con conserva</Link></li>
                                 </ul>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="##">Blog</a>
+                                <Link className="nav-link" to="##">Blog</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="##">Solicita tu crédito</a>
+                                <Link className="nav-link" to="##">Solicita tu crédito</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="##">Contáctanos</a>
+                                <Link className="nav-link" to="##">Contáctanos</Link>
                             </li>
                             <li className="nav-item">
                                 <button type="button" className="btn-ingresa">Ingresar</button>
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -3,46 +3,48 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import conserva from '../img/Conserva.svg'
 import '../styles/navBar.css'
+import { Link } from "react-router-dom";
+
 
 const NavBar = () => {
   return (
     <nav className="navbar navbar-white navbar-expand-md borderNav py-0">
       <div className="container-fluid">
-        <a class="navbar-brand" href="#">
+        <Link class="navbar-brand" to="#">
           <img src={conserva} alt="" width="140" height="70" class="d-inline-block align-text-top" />
-        </a>
+        </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarText">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Inicio</a>
+              <Link  className="nav-link active" aria-current="page" to="/">Inicio</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/about">Nosotros</a>
+              <Link className="nav-link" to="/about" >Nosotros</Link>
             </li>
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <Link class="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Productos
-              </a>
+              </Link>
               <ul class="dropdown-menu dropdown-menu-light">
-                <li><a class="dropdown-item mdp" href="#">Mujeres de Palabra</a></li>
-                <li><a class="dropdown-item cta" href="#">Conserva T-activa</a></li>
-                <li><a class="dropdown-item tcc" href="#">Tu negocio con conserva</a></li>
+                <li><Link class="dropdown-item mdp" to="#">Mujeres de Palabra</Link></li>
+                <li><Link class="dropdown-item cta" to="#">Conserva T-activa</Link></li>
+                <li><Link class="dropdown-item tcc" to="#">Tu negocio con conserva</Link></li>
               </ul>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Blog</a>
+              <Link className="nav-link" to="#">Blog</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Solicita tu crédito</a>
+              <Link className="nav-link" to="#">Solicita tu crédito</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Contáctanos</a>
+              <Link className="nav-link" to="#">Contáctanos</Link>
             </li>
             <li className="nav">
-              <a className="nav-link btn btn-ingresar" href="#">Ingresa</a>
+              <Link className="nav-link btn btn-ingresar" to="#">Ingresa</Link>
             </li>
           </ul>
         </div>
